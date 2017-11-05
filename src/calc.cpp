b030ed79f47066a30f39199ee582916cc7ef982b
Lucas Racoci <racoci.0@gmail.com>
2017-11-05 14:23:39 -0200

    Agora o codigo ta funcionando confia no pai

---
+++ src/calc.cpp
@@ -0,0 +1,72 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+#define MAT(T) vector < vector <T> >
+
+/* Programa que calcula o custo de uma solucao, utilizando os
+ * dados de entrada e tambem o vetor solucao do branch and bound */
+int main() {
+	int i, j, n, m;
+	MAT(int) T, P;
+	vector < int > cost;
+	vector < int > sol;
+	vector < int > part;
+	int c = 0;
+
+	cin >> n >> m;
+	T.resize(m);
+	for (i = 0; i < m; i++) {
+		T[i].resize(n);
+		for (j = 0; j < n; j++){
+			cin >> T[i][j];
+		}
+	}
+
+	cost.resize(m);
+	for (i = 0; i < m; i++) {
+		cin >> cost[i];
+	}
+
+	sol.resize(n);
+	for (i = 0; i < n; i++) {
+		cin >> sol[i];
+		sol[i] -= 1;
+	}
+
+	P.resize(m);
+	for (i = 0; i < m; i++) {
+		P[i].resize(n);
+		for (j = 0; j < n; j++){
+			P[i][j] = T[i][sol[j]];
+		}
+	}
+
+	part.resize(m, 0);
+	for(int i = 0; i < m; i++){
+		for (int j = 0; j < n; j++){
+			part[i] += T[i][j];
+		}
+	}
+
+	for (i = 0; i < m; i++) {
+		int aux = 0;
+		while (P[i][aux] != 1) {
+			aux++;
+		}
+		for (j = aux; j < n; j++){
+			if (P[i][j] == 1) {
+				part[i] -= 1;
+			} else {
+				if (part[i] > 0) {
+					c += cost[i];
+				}
+			}
+		}
+	}
+
+	cout << c << endl;
+
+	return 0;
+}
