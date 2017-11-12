1d7a42357045809c2b242e0501e8ec5f9914af8e
Lucas <ra156331@students.ic.unicamp.br>
2017-11-12 00:47:20 -0200

    Erro muito estranho na compilação

--- src/bnb.cpp
+++ src/bnb.cpp
@@ -1,7 +1,14 @@
+/*
+	Nome (RA)
+	Lucas Alves Racoci (156331)
+	Luiz Fernando Fonseca (156475)
+*/
 #include <csignal>
 #include <cstdlib>
 #include <iostream>
 #include <vector>
+#include <limits.h>
+#include <queue>
 
 using namespace std;
 
@@ -9,25 +16,33 @@ using namespace std;
 volatile int pare = 0;       // Indica se foi recebido o sinal de interrupcao
 volatile int escrevendo = 0; // Indica se estah atualizando a melhor solucao
 
+/* Definicoes de macros que facilitam o desenvolvimento do codigo */
+#define INF INT_MAX
+#define MAT(T) vector< vector <T> >
+
 // Variaveis para guardar a melhor solucao encontrada
 vector<int> melhor_solucao;
 int melhor_custo;
 
-void imprime_saida() {
+void imprime_saida()
+{
     // Lembre-se: a primeira linha da saida deve conter n inteiros,
     // tais que o j-esimo inteiro indica o dia de gravacao da cena j!
     for (int j = 0; j < melhor_solucao.size(); j++)
         cout << melhor_solucao[j] << " ";
     // A segunda linha contem o custo (apenas de dias de espera!)
-    cout << endl << melhor_custo << endl;
+    cout << endl
+         << melhor_custo << endl;
 }
 
-void atualiza_solucao(const vector<int> &solucao, int custo) {
+void atualiza_solucao(const vector<int> &solucao, int custo)
+{
     escrevendo = 1;
     melhor_solucao = solucao;
     melhor_custo = custo;
     escrevendo = 0;
-    if (pare == 1) {
+    if (pare == 1)
+    {
         // Se estava escrevendo a solucao quando recebeu o sinal,
         // espera terminar a escrita e apenas agora imprime a saida e
         // termina
@@ -36,22 +51,258 @@ void atualiza_solucao(const vector<int> &solucao, int custo) {
     }
 }
 
-void interrompe(int signum) {
+void interrompe(int signum)
+{
     pare = 1;
-    if (escrevendo == 0) {
+    if (escrevendo == 0)
+    {
         // Se nao estava escrevendo a solucao, pode imprimir e terminar
         imprime_saida();
         exit(0);
     }
 }
+/*Variáveis que definem o problema que esta sendo resolvido e guarda as suas informacoes */
 
-int main() {
+int n, // number of scenes
+    m; // number of actors
+
+MAT(int)
+T;                  // m x n
+vector<int> cost;   // Vetor com os custos das cenas
+vector<int> part;   // Vetor com a participacao dos atores
+int bestCost = INF; // Melhor custo encontrado
+
+/* Calcula os s_i's do enunciado */
+void compute_part();
+
+/* Funcao que executa o branch and bound */
+void solve();
+
+/* Calcula os s_i's do enunciado */
+void compute_part()
+{
+    part.resize(m, 0);
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            part[i] += T[i][j];
+        }
+    }
+}
+
+/* Classe que representa uma atribuicao de cenas aos dias */
+class Schedule {
+  public:
+    int depth;                  // Nivel na arvore de busca
+    int l, r;                   // Indicam os locais de insercao de novas escolhas
+    int boundVal;               // Limitante inferior da solucao
+    bool complete;              // Indica se a solucao e completa
+    vector<int> days;           // Permutacao das cenas
+    vector<int> chosen;         // Indica quais cenas ja foram escolhidas
+    vector<int> first;          // Indica o primeiro dia de cada ator
+    vector<int> last;           // Indica o ultimo dia de cada ator
+    vector<Schedule> offspring; // Cria os filhos do no atual
+
+    /* Cria o objeto inicial de Schedule */
+    Schedule()
+    {
+        depth = 0;
+        l = 0;
+        r = n - 1;
+        boundVal = 0;
+        complete = false;
+        days.resize(n, -1);
+        chosen.resize(n, false);
+        first.resize(m, n);
+        last.resize(m, -1);
+    }
+
+    /* Cria objetos subsequentes de Schedule a partir de outro ja criado */
+    Schedule(Schedule *dad, int choice) : 
+        days(dad->days),
+        chosen(dad->chosen),
+        first(dad->first),
+        last(dad->last)
+    {
+        /* Incrementa o nivel do no atual em relacao ao seu superior */
+        depth = dad->depth + 1;
+        complete = depth == n;
+        chosen[choice] = true;
+
+        /* Checa o lado do vetor que a nova escolha sera inserida,
+		* atualiza os vetores que indicam o primeiro e ultimo dia
+		* de cada ator e atualiza os indices direito e esquerdo */
+        int chosen_day;
+        if (depth % 2 == 1)
+        {
+            r = dad->r;
+            l = dad->l + 1;
+            chosen_day = l - 1;
+        }
+        else
+        {
+            l = dad->l;
+            r = dad->r - 1;
+            chosen_day = r + 1;
+        }
+
+        days[chosen_day] = choice;
+        for (int i = 0; i < m; i++)
+        {
+            if (T[i][choice])
+            {
+                if (chosen_day < first[i])
+                {
+                    first[i] = chosen_day;
+                }
+                if (chosen_day > last[i])
+                {
+                    last[i] = chosen_day;
+                }
+            }
+        }
+    }
+
+    /* Retorna os filhos do no corrente */
+    vector<Schedule> &branch()
+    {
+
+        for (int j = 0; j < n; j++)
+        {
+            if (not chosen[j])
+            {
+                Schedule child(this, j);
+                offspring.push_back(child);
+            }
+        }
+
+        return offspring;
+    }
+
+    /* Calcula o limitante do no */
+    void bound()
+    {
+        int val = 0;
+
+        for (int i = 0; i < m; i++)
+        {
+            // if i in A(P) <==> di != none != ei
+            if (first[i] <= l and last[i] >= r)
+            {
+                val += cost[i] * (last[i] - first[i] + 1 - part[i]);
+            }
+        }
+
+        boundVal = val;
+    }
+
+    void updateBest()
+    {
+        vector<int> scenes;
+        int tam = days.size();
+        scenes.resize(tam);
+        for (int j = 0; j < tam; j++)
+        {
+            scenes[days[j]] = j;
+        }
+        atualiza_solucao(scenes, bestCost);
+    }
+} best;
+//Schedule best;      // Melhor solucao encontrada
+
+/* Funcao de comparacao para se obter um heap de minimo */
+struct compare {
+    bool operator()(const Schedule &l, const Schedule &r)
+    {
+        return l.boundVal > r.boundVal;
+    }
+};
+
+
+/* Funcao que executa o branch and bound */
+void solve()
+{
+    //s_i's do enunciado
+    compute_part();
+
+    /* Fila de prioridade que contera os nos ativos */
+    Schedule root();
+    priority_queue<Schedule, vector<Schedule>, compare> pq;
+    pq.push(root);
+
+    /* Explora os nos ativos */
+    while (not pq.empty())
+    {
+        /* Retira o melhor no da fila */
+        Schedule k = pq.top();
+        pq.pop();
+
+        /* Checa se o no atual pode dar uma solucao melhor
+		 * do que a melhor encontrada ate o momento */
+        if (k.boundVal < bestCost)
+        {
+            /* Gera os filhos do no ativo, calcula os limitantes e continua o processo */
+            vector<Schedule> &offspring = k.branch();
+            /* Trata cada filho separadamente */
+            for (int j = 0; j < offspring.size(); j++)
+            {
+                offspring[j].bound();
+                if (offspring[j].boundVal < bestCost)
+                {
+                    if (offspring[j].complete)
+                    {
+                        bestCost = offspring[j].boundVal;
+                        best = offspring[j];
+                    }
+                    else
+                    {
+                        pq.push(offspring[j]);
+                    }
+                }
+            }
+        }
+    }
+}
+
+/* Funcao que faz a entrada dos dados e os coloca dentro de uma classe problema */
+void le_entrada()
+{
+    int i, j;
+
+    cin >> n >> m;
+    T.resize(m);
+    for (i = 0; i < m; i++){
+        T[i].resize(n);
+        for (j = 0; j < n; j++) {
+            cin >> T[i][j];
+        }
+    }
+
+    cost.resize(m);
+    for (i = 0; i < m; i++) {
+        cin >> cost[i];
+    }
+
+}
+
+int main()
+{
     // Registra a funcao que trata o sinal
     signal(SIGINT, interrompe);
 
     // Continue sua implementacao aqui. Sempre que encontrar uma nova
     // solucao, utilize a funcao atualiza_solucao para atualizar as
     // variaveis globais.
+
+    /* Le os dados do problema */
+    le_entrada();
+
+    /* Resolve o problema */
+    solve();
+
+    /* Imprime os resultados */
+    imprime_saida();
 
     return 0;
 }
